c37e6f41b4cd4437393b16b0b0c5ae769283f9f9
Manab Debnath <manab_debnath@outlook.com>
2024-09-01 01:31:31 +0530

    complete profile update page

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,18 +1,41 @@
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { createContext, useState } from "react";
 import { db, auth } from "../config/firebase";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
 const AppContextProvider = (props) => {
 	const [userData, setUserData] = useState(null);
 	const [chatData, setChatData] = useState(null);
+	const navigate = useNavigate()
 
 	const loadUserData = async (uid) => {
 		try {
 			const userRef = doc(db, "users", uid);
 			const userSnap = await getDoc(userRef);
 			const userData = userSnap.data();
-			console.log(userData);
+			setUserData(userData);
+
+			//! redirected to chat page if name and avatar is uploaded
+			if(userData.avatar && userData.name)	{
+				navigate('/chat')
+			}
+			else	{
+				navigate('/profile')
+			}
+
+			await updateDoc(userRef, {
+				lastSeen: Date.now()
+			})
+
+			setInterval(async () => {
+				if(auth)	{
+					await updateDoc(userRef, {
+						lastSeen: Date.now()
+					})							
+				}
+			}, 60000)
+
 
 		} catch (error) {}
 	};
--- src/pages/profileUpdate/ProfileUpdate.jsx
+++ src/pages/profileUpdate/ProfileUpdate.jsx
@@ -1,17 +1,86 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AvatarIcon from "../../assets/avatar_icon.png";
 import ChatIcon from "../../assets/chatIcon.png";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth, db } from "../../config/firebase";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import upload from "../../lib/upload";
 
 const ProfileUpdate = () => {
 
+	const navigate = useNavigate()
 	const [image, setImage] = useState(false)
+	const [name, setName] = useState("")
+	const [bio, setBio] = useState("")
+	const [uid, setUid] = useState("")
+	const [prevImg, setPrevImg] = useState("")
+
+
+	const profileUpdate = async (e) => {
+		e.preventDefault();
+		
+		try	{
+			if(!prevImg && !image)	{
+				toast.error("Upload profile picture")
+			}
+
+			const docRef = doc(db, "users", uid);
+
+			if(image)	{
+				const imgUrl = await upload(image)
+				setPrevImg(imgUrl)
+				await updateDoc(docRef, {
+					avatar: imgUrl,
+					bio:bio,
+					name:name,
+				})
+			}
+			else	{
+
+			}
+
+		} catch(error)	{
+
+		}
+	}
+
+	useEffect(() => {
+		onAuthStateChanged(auth, async (user) => {
+			if(user)	{
+				setUid(user.uid)
+				const docRef = doc(db, "users", user.uid)
+				const docSnap = await getDoc(docRef)
+				console.log(docSnap.data);
+				
+
+				if(docSnap.data().name)	{
+					setName(docSnap.data.name)
+					console.log("name fetch");
+					
+				}
+				if(docSnap.data().bio)	{
+					setBio(docSnap.data().bio)
+					console.log("bio fetch");
+					
+				}
+				if(docSnap.data().avatar)	{
+					setPrevImg(docSnap.data().avatar)
+				}
+			}
+			else	{
+				navigate('/')
+			}
+		})
+	}, [])
 
 	return (
 		<div className="min-h-screen flex items-center justify-center">
 			{" "}
 			{/* profile */}
 			<div className="bg-white flex items-center justify-between min-w-700 rounded-md">
-				<form action="" className="flex flex-col gap-5 p-10">
+				<form action="" className="flex flex-col gap-5 p-10" onSubmit={profileUpdate}>
 					<h3 className="font-medium">Profile Details</h3>
 					<label
 						htmlFor="avatar"
@@ -37,11 +106,15 @@ const ProfileUpdate = () => {
 						placeholder="Your name"
 						required
 						className="p-2 min-w-72 focus:outline-none focus:ring-2 focus:ring-custom-blue focus:border-transparent"
+						onChange={(e) => setName(e.target.value)}
+						value={name}
 					/>
 					<textarea
 						placeholder="Write Profile Bio..."
 						required
 						className="p-2 min-w-72 focus:outline-none focus:ring-2 focus:ring-custom-blue focus:border-transparent"
+						onChange={(e) => setBio(e.target.value)}
+						value={bio}
 					></textarea>
 					<button
 						type="submit"
